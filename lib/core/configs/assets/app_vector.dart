ba4f3cb7103feb5e0ae4ba688c1e1463449eed85
Raehyeong <lrh1439@gmail.com>
2024-10-10 19:14:57 -0400

    Delete lib directory
    delete previous codes

--- lib/core/configs/assets/app_vector.dart
+++
@@ -1,9 +0,0 @@
-class AppVectors {
-  static const String basePath = 'assets/vectors/';
-  static const String format = '.svg';
-
-  static const String logo = '${basePath}SpotifyLogo$format';
-  static const String sun = '${basePath}sun$format';
-  static const String moon = '${basePath}moon$format';
-  static const String settings = '${basePath}settings$format';
-}
--- lib/core/configs/theme/app_color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColor {
-  static const primary = Color.fromARGB(255, 29, 185, 84);
-  static const lightBackground = Color.fromARGB(255, 255, 255, 255);
-  static const darkBackground = Color.fromARGB(255, 0, 0, 0);
-  static const barGray = Color.fromARGB(255, 36, 35, 35);
-  static const textGray = Color.fromARGB(255, 55, 53, 53);
-}
