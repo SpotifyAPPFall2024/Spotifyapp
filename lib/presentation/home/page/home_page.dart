b1fa9bd464127b11edd943fd7b90eddd33227d7c
Raehyeong <lrh1439@gmail.com>
2024-12-02 14:38:40 -0500

    Update home_page.dart

--- lib/presentation/home/page/home_page.dart
+++ lib/presentation/home/page/home_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
 import 'package:spotifyapp/common/helpers/dark_mode.dart';
 import 'package:spotifyapp/core/utils/authentication_service.dart';
 import 'package:spotifyapp/presentation/features/widgets/collab.dart';
 import 'package:spotifyapp/presentation/home/page/library_page.dart';
 import 'package:spotifyapp/presentation/home/page/player_page.dart';
 import 'package:spotifyapp/presentation/home/page/search_page.dart';
+import 'package:spotifyapp/presentation/home/page/popup_screen.dart';
 
 import '../../../core/configs/assets/app_vector.dart';
 
@@ -18,12 +20,23 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   int currentNavigation = 0;
   late Future<List<dynamic>> featuredPlaylist;
   late Future<List<dynamic>> recentPlays;
   late Future<List<dynamic>> topMixes;
   late Future<List<dynamic>> jumpBackIn;
+  
+  late AnimationController _queueController;
+
+  bool _isQueueVisible = false;
+  bool _isPopupVisible = false;
+
+  void _togglePopup() {
+    setState(() {
+      _isPopupVisible = !_isPopupVisible;
+    });
+  }
 
   @override
   void initState() {
@@ -33,6 +46,43 @@ class _HomePageState extends State<HomePage> {
     recentPlays = AuthenticationService().fetchRecentPlays(widget.accessToken);
     topMixes = AuthenticationService().fetchTopMixes(widget.accessToken);
     jumpBackIn = AuthenticationService().fetchJumpBackIn(widget.accessToken);
+
+    _queueController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+  }
+
+Future<void> addToQueue(String trackUri, String accessToken) async {
+    final response = await http.post(
+      Uri.parse('https://api.spotify.com/v1/me/player/queue?uri=$trackUri'),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 204) {
+      print('Song added to queue');
+    } else {
+      throw Exception('Failed to add song to queue');
+    }
+  }
+  
+@override
+  void dispose() {
+    _queueController.dispose();
+    super.dispose();
+  }
+
+  void _toggleQueue() {
+    setState(() {
+      _isQueueVisible = !_isQueueVisible;
+      if (_isQueueVisible) {
+        _queueController.forward();
+      } else {
+        _queueController.reverse();
+      }
+    });
   }
 
   @override
@@ -54,13 +104,31 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(
               Icons.account_circle,
               color: context.isDarkMode
-                  ? Colors.white.withOpacity(0.03)
-                  : Colors.black.withOpacity(0.03),
+                  ? const Color(0xFF1DB954)
+                  : const Color(0xFF191414),
+            ),
+          ),
+          IconButton( // Queue Icon
+            onPressed: _togglePopup,
+            icon:Icon (
+              Icons.queue_music,
+              color: context.isDarkMode
+                ? const Color(0xFF1DB954)
+                : const Color(0xFF191414),
             ),
           ),
         ],
       ),
-      body: buildBody(),
+      body: Stack(
+        children: [
+          buildBody(),
+          PopupScreen(isVisible: _isPopupVisible,
+          accessToken: widget.accessToken,
+          playlistID: '44ydl0IWV156LjtcFKDrep', //put your PLaylistId here. 
+          //(You can find your playlist id from the url of actual playlist page)
+          ),
+        ],
+      ),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
@@ -105,10 +173,8 @@ class _HomePageState extends State<HomePage> {
         return LibraryPage(accessToken: widget.accessToken);
       case 3: //Collab Feature
         return CollabPage(
-          userName:
-              'YOUR_USER_NAME', // Replace with actual username variable if available
-          userFollowers:
-              'YOUR_FOLLOWERS_COUNT', // Replace with actual followers variable if available
+          userName: 'YOUR_USER_NAME',          // Replace with actual username variable if available
+          userFollowers: 'YOUR_FOLLOWERS_COUNT', // Replace with actual followers variable if available
           accessToken: widget.accessToken,
         );
       default:
@@ -117,87 +183,88 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget buildHomeContent() {
-    return ListView(
-      padding: const EdgeInsets.all(16.0),
-      children: [
-        buildSectionTitle('Recommended Plays'),
-        FutureBuilder<List<dynamic>>(
-          future: topMixes,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(
-                  child: Text('No recommended plays available'));
-            } else {
-              final data = snapshot.data!;
-              return buildGrid(data);
-            }
-          },
-        ),
-        const SizedBox(height: 24),
-        buildSectionTitle('Recent Plays'),
-        FutureBuilder<List<dynamic>>(
-          future: recentPlays,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(child: Text('No recent plays available'));
-            } else {
-              final data = snapshot.data!;
-              return buildGrid(data);
-            }
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget buildSectionTitle(String title) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-  }
-
-  Widget buildGrid(List<dynamic> tracks) {
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
-      ),
-      itemCount: tracks.length,
-      itemBuilder: (context, index) {
-        final track =
-            tracks[index]['track'] ?? tracks[index]; // Adjust as needed
-        return buildTrackCard(track);
+    return FutureBuilder(
+      future: featuredPlaylist,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text("Error: ${snapshot.error}"));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No data available'));
+        } else {
+          final playlists = snapshot.data!;
+          return ListView(
+            children: [
+              Container(
+                height: 200,
+                color: Colors.black,
+                child: PageView(
+                  children: playlists.map<Widget>((playlist) {
+                    final imageUrl = playlist['images'][0]['url'];
+                    return Image.network(imageUrl, fit: BoxFit.contain);
+                  }).toList(),
+                ),
+              ),
+              buildSection('Recent Plays', recentPlays),
+            ],
+          );
+        }
       },
+    );
+  }
+
+  Widget buildSection(String title, Future<List<dynamic>> items) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title,
+              style:
+                  const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 16),
+          FutureBuilder<List<dynamic>>(
+            future: items,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                final data = snapshot.data!;
+                return GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                  ),
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    final track = data[index]['track'];
+                    return track != null
+                        ? buildTrackCard(track)
+                        : const SizedBox();
+                  },
+                );
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 
   Widget buildTrackCard(dynamic track) {
     final imageUrl = track['album']?['images']?.first?['url'];
     final trackID = track['id'];
-    if (track == null) {
-      return const SizedBox();
-    }
+    final trackUri = track['uri'];
 
     return GestureDetector(
       onTap: () {
+        addToQueue(trackUri, widget.accessToken);
         Navigator.push(
           context,
           MaterialPageRoute(
